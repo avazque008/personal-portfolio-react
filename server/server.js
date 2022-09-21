708a030148a4f85ad47c07157967614d4aec4bf2
Alex Vazquez <avazque008@outlook.com>
2022-09-21 15:26:05 -0400

    replaced syntax

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
